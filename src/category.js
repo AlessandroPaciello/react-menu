e92bf6b3a94b8a1dfbafeee05304e5d08da48ffc
AlessandroPaciello <alessandro.paciello25@gmail.com>
2021-06-01 09:00:24 +0200

    01/06/21

--- src/category.js
+++ src/category.js
@@ -76,7 +76,7 @@ class Category extends React.Component {
         
         let element = menu.category.map((ob, index) => {
             return (
-                <div data-scroll key={"element" + index} index={index} id={"element" + index} className='list animate__animated' onTouchEnd={e => this.Openlist(e)}>
+                <div key={"element" + index} index={index} id={"category"} className='list animate__animated' onTouchEnd={e => this.Openlist(e)}>
                     {ob.nameCategory}
                 </div>
                 ) 
