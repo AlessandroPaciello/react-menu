b0ef4be724b90f6861d8200961c1f48f01ffccfd
AlessandroPaciello <alessandro.paciello25@gmail.com>
2021-06-07 13:42:54 +0200

    save 07/06/21

--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import Category from './category'
 const menu = require('./menu.json')
 
 class Menu extends React.Component {
+
+    
 
     NameMenu = () => {
 
@@ -10,29 +13,31 @@ class Menu extends React.Component {
         let description_menu = menu.category[index].menu.Description
         let Name_menu
 
-        if(menu.category[index].menu.Description){
-            Name_menu = menu.category[index].menu.name.map((el, index_map) => {
+        Name_menu = menu.category[index].menu.name.map((el, index_map) => {
+            if(menu.category[index].menu.Category && menu.category[index].menu.Category.hasOwnProperty(index_map)){
                 return (
                     <li key={index_map} className='menu_list_description'>
-                        <div key={index_map} className='text-list'>
+                    <div className='menu_category'>{menu.category[index].menu.Category[index_map]}</div>
+                    <div key={index_map} className='text_list add_category'>
+                            <div className='menu_name'>{el}</div>
+                            <div className='menu_price'>{price_menu[index_map]}</div>
+                        </div>
+                    <div className="description_menu add_category">{description_menu[index_map]}</div>
+                </li>)
+            }
+            else{
+                return (
+                    <li key={index_map} className='menu_list_description'>
+                        <div key={index_map} className='text_list'>
                             <div className='menu_name'>{el}</div>
                             <div className='menu_price'>{price_menu[index_map]}</div>
                         </div>
                         <div className="description_menu">{description_menu[index_map]}</div>
                     </li>)
-            })
-        }
-        else{
-            Name_menu = menu.category[index].menu.name.map((el, index) => {
-                return (
-                    <li key={index} className='menu_list'>
-                        <div key={index} className='menu_name'>{el}</div>
-                        <div className='menu_price'>{price_menu[index]}</div>
-                    </li>)
-            })
-        }
-        
+            }
 
+        })
+        
         return Name_menu
     }
 
@@ -58,6 +63,8 @@ class Menu extends React.Component {
                </div>
     }
 }
+
+
 
 
 export default Menu
